fd1302b736919aa0e7d24374f2fef9c3eccdac96
Matt Cerisano <mattcerisano@Hyrule-Castle.local>
2026-02-25 16:35:54 -0500

    UI: Refine About section copy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -674,7 +674,7 @@ function About() {
             We&apos;re the kind of team that can pivot on a dime and still make it look intentional. Big, glossy TV spots? Done. TikToks that don&apos;t feel like ads? Absolutely. Social campaigns, branded docs, narrative pieces&mdash;this is our playground.
           </p>
           <p>
-            And sure, we&apos;ve picked up some awards and worked with names you&apos;d definitely recognize&mdash;but that&apos;s not really the point. What matters is showing up, getting it right, and keeping the vibe good. Clients tend to like having us around, and we like keeping it that way.
+            And sure, we&apos;ve picked up some awards and worked with names you&apos;d definitely recognize&mdash;but that&apos;s not the point. What matters is showing up, getting it done, and keeping the vibe good. Clients tend to like having us around, and we like keeping it that way.
           </p>
         </div>
       </div>
